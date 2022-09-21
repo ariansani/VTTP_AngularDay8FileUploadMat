1e7cd3b7047f171bc28afb969546a2158f380ee4
Arian Sani <ariansani@hotmail.com>
2022-09-21 11:30:56 +0800

    added fileupload and angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularmaterialtut';
+
+  @ViewChild(MatDrawer)
+  drawer!: MatDrawer
+
+  form!:FormGroup
+
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  toggleSidebar(){
+    console.info("test")
+    this.drawer.toggle()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegisterComponent } from './components/register.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UploadComponent } from './components/upload.component';
+import { UploadService } from './services/upload.service';
+
+const appRoutes: Routes = [
+  {path:'', component:RegisterComponent},
+  {path:'upload', component:UploadComponent},
+  {path:'**', redirectTo:'/', pathMatch:'full'}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    UploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule, FormsModule,
+    RouterModule.forRoot(appRoutes,{useHash:true}),
   ],
-  providers: [],
+  providers: [UploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
